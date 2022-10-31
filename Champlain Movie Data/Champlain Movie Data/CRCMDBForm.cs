cf97d208e931e827fc48b642786061e8a7428f3b
vlad <estevez.vladimir@yahoo.com>
2022-10-31 13:58:01 -0400

    Committing Display Movie Form with datasets

--- Champlain Movie Data/Champlain Movie Data/CRCMDBForm.cs
+++ Champlain Movie Data/Champlain Movie Data/CRCMDBForm.cs
@@ -16,5 +16,14 @@ namespace Champlain_Movie_Data
         {
             InitializeComponent();
         }
+
+        private void moviesButton_Click(object sender, EventArgs e)
+        {
+            DisplayMoviesForm details = new DisplayMoviesForm();
+
+            details.ShowDialog();
+
+            
+        }
     }
 }
---
+++ Champlain Movie Data/Champlain Movie Data/DisplayMoviesForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Champlain_Movie_Data
+{
+    public partial class DisplayMoviesForm : Form
+    {
+        public DisplayMoviesForm()
+        {
+            InitializeComponent();
+        }
+
+        private void movieBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.movieBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.movieDatabaseDataSet);
+
+        }
+
+        private void DisplayMoviesForm_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'movieDatabaseDataSet.Movie' table. You can move, or remove it, as needed.
+            this.movieTableAdapter.Fill(this.movieDatabaseDataSet.Movie);
+
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
