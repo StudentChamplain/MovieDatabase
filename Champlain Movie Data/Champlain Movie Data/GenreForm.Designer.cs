630c2830536ca6a01e897cac0120d7ec24cd78a7
2088826 <LE2088826@crc-lennox.qc.ca>
2022-11-02 09:46:15 -0400

    Added GenreForm

---
+++ Champlain Movie Data/Champlain Movie Data/GenreForm.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace Champlain_Movie_Data
+{
+    partial class GenreForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GenreForm));
+            System.Windows.Forms.Label genreIdLabel;
+            System.Windows.Forms.Label genreLabel;
+            this.movieDatabaseDataSet = new Champlain_Movie_Data.MovieDatabaseDataSet();
+            this.genreBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.genreTableAdapter = new Champlain_Movie_Data.MovieDatabaseDataSetTableAdapters.GenreTableAdapter();
+            this.tableAdapterManager = new Champlain_Movie_Data.MovieDatabaseDataSetTableAdapters.TableAdapterManager();
+            this.genreBindingNavigator = new System.Windows.Forms.BindingNavigator(this.components);
+            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
+            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
+            this.genreBindingNavigatorSaveItem = new System.Windows.Forms.ToolStripButton();
+            this.genreIdTextBox = new System.Windows.Forms.TextBox();
+            this.genreTextBox = new System.Windows.Forms.TextBox();
+            genreIdLabel = new System.Windows.Forms.Label();
+            genreLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.movieDatabaseDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.genreBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.genreBindingNavigator)).BeginInit();
+            this.genreBindingNavigator.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // movieDatabaseDataSet
+            // 
+            this.movieDatabaseDataSet.DataSetName = "MovieDatabaseDataSet";
+            this.movieDatabaseDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // genreBindingSource
+            // 
+            this.genreBindingSource.DataMember = "Genre";
+            this.genreBindingSource.DataSource = this.movieDatabaseDataSet;
+            // 
+            // genreTableAdapter
+            // 
+            this.genreTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.ActorTableAdapter = null;
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.GenreTableAdapter = this.genreTableAdapter;
+            this.tableAdapterManager.MovieGenreTableAdapter = null;
+            this.tableAdapterManager.MovieTableAdapter = null;
+            this.tableAdapterManager.RoleTableAdapter = null;
+            this.tableAdapterManager.UpdateOrder = Champlain_Movie_Data.MovieDatabaseDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // genreBindingNavigator
+            // 
+            this.genreBindingNavigator.AddNewItem = this.bindingNavigatorAddNewItem;
+            this.genreBindingNavigator.BindingSource = this.genreBindingSource;
+            this.genreBindingNavigator.CountItem = this.bindingNavigatorCountItem;
+            this.genreBindingNavigator.DeleteItem = this.bindingNavigatorDeleteItem;
+            this.genreBindingNavigator.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.genreBindingNavigator.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bindingNavigatorMoveFirstItem,
+            this.bindingNavigatorMovePreviousItem,
+            this.bindingNavigatorSeparator,
+            this.bindingNavigatorPositionItem,
+            this.bindingNavigatorCountItem,
+            this.bindingNavigatorSeparator1,
+            this.bindingNavigatorMoveNextItem,
+            this.bindingNavigatorMoveLastItem,
+            this.bindingNavigatorSeparator2,
+            this.bindingNavigatorAddNewItem,
+            this.bindingNavigatorDeleteItem,
+            this.genreBindingNavigatorSaveItem});
+            this.genreBindingNavigator.Location = new System.Drawing.Point(0, 0);
+            this.genreBindingNavigator.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
+            this.genreBindingNavigator.MoveLastItem = this.bindingNavigatorMoveLastItem;
+            this.genreBindingNavigator.MoveNextItem = this.bindingNavigatorMoveNextItem;
+            this.genreBindingNavigator.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
+            this.genreBindingNavigator.Name = "genreBindingNavigator";
+            this.genreBindingNavigator.PositionItem = this.bindingNavigatorPositionItem;
+            this.genreBindingNavigator.Size = new System.Drawing.Size(487, 27);
+            this.genreBindingNavigator.TabIndex = 0;
+            this.genreBindingNavigator.Text = "bindingNavigator1";
+            // 
+            // bindingNavigatorMoveFirstItem
+            // 
+            this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
+            this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
+            this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(29, 22);
+            this.bindingNavigatorMoveFirstItem.Text = "Move first";
+            // 
+            // bindingNavigatorMovePreviousItem
+            // 
+            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
+            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
+            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(29, 22);
+            this.bindingNavigatorMovePreviousItem.Text = "Move previous";
+            // 
+            // bindingNavigatorSeparator
+            // 
+            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorPositionItem
+            // 
+            this.bindingNavigatorPositionItem.AccessibleName = "Position";
+            this.bindingNavigatorPositionItem.AutoSize = false;
+            this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
+            this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 27);
+            this.bindingNavigatorPositionItem.Text = "0";
+            this.bindingNavigatorPositionItem.ToolTipText = "Current position";
+            // 
+            // bindingNavigatorCountItem
+            // 
+            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
+            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(45, 20);
+            this.bindingNavigatorCountItem.Text = "of {0}";
+            this.bindingNavigatorCountItem.ToolTipText = "Total number of items";
+            // 
+            // bindingNavigatorSeparator1
+            // 
+            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 6);
+            // 
+            // bindingNavigatorMoveNextItem
+            // 
+            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
+            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
+            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(29, 24);
+            this.bindingNavigatorMoveNextItem.Text = "Move next";
+            // 
+            // bindingNavigatorMoveLastItem
+            // 
+            this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
+            this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
+            this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(29, 24);
+            this.bindingNavigatorMoveLastItem.Text = "Move last";
+            // 
+            // bindingNavigatorSeparator2
+            // 
+            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 6);
+            // 
+            // bindingNavigatorAddNewItem
+            // 
+            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
+            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
+            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(29, 24);
+            this.bindingNavigatorAddNewItem.Text = "Add new";
+            // 
+            // bindingNavigatorDeleteItem
+            // 
+            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
+            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
+            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(29, 24);
+            this.bindingNavigatorDeleteItem.Text = "Delete";
+            // 
+            // genreBindingNavigatorSaveItem
+            // 
+            this.genreBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.genreBindingNavigatorSaveItem.Image = ((System.Drawing.Image)(resources.GetObject("genreBindingNavigatorSaveItem.Image")));
+            this.genreBindingNavigatorSaveItem.Name = "genreBindingNavigatorSaveItem";
+            this.genreBindingNavigatorSaveItem.Size = new System.Drawing.Size(23, 23);
+            this.genreBindingNavigatorSaveItem.Text = "Save Data";
+            this.genreBindingNavigatorSaveItem.Click += new System.EventHandler(this.genreBindingNavigatorSaveItem_Click);
+            // 
+            // genreIdLabel
+            // 
+            genreIdLabel.AutoSize = true;
+            genreIdLabel.Location = new System.Drawing.Point(85, 102);
+            genreIdLabel.Name = "genreIdLabel";
+            genreIdLabel.Size = new System.Drawing.Size(61, 16);
+            genreIdLabel.TabIndex = 1;
+            genreIdLabel.Text = "Genre Id:";
+            // 
+            // genreIdTextBox
+            // 
+            this.genreIdTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.genreBindingSource, "GenreId", true));
+            this.genreIdTextBox.Location = new System.Drawing.Point(152, 99);
+            this.genreIdTextBox.Name = "genreIdTextBox";
+            this.genreIdTextBox.Size = new System.Drawing.Size(100, 22);
+            this.genreIdTextBox.TabIndex = 2;
+            // 
+            // genreLabel
+            // 
+            genreLabel.AutoSize = true;
+            genreLabel.Location = new System.Drawing.Point(85, 130);
+            genreLabel.Name = "genreLabel";
+            genreLabel.Size = new System.Drawing.Size(47, 16);
+            genreLabel.TabIndex = 3;
+            genreLabel.Text = "Genre:";
+            // 
+            // genreTextBox
+            // 
+            this.genreTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.genreBindingSource, "Genre", true));
+            this.genreTextBox.Location = new System.Drawing.Point(152, 127);
+            this.genreTextBox.Name = "genreTextBox";
+            this.genreTextBox.Size = new System.Drawing.Size(100, 22);
+            this.genreTextBox.TabIndex = 4;
+            // 
+            // GenreForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 322);
+            this.Controls.Add(genreIdLabel);
+            this.Controls.Add(this.genreIdTextBox);
+            this.Controls.Add(genreLabel);
+            this.Controls.Add(this.genreTextBox);
+            this.Controls.Add(this.genreBindingNavigator);
+            this.Name = "GenreForm";
+            this.Text = "Genres";
+            this.Load += new System.EventHandler(this.GenreForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.movieDatabaseDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.genreBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.genreBindingNavigator)).EndInit();
+            this.genreBindingNavigator.ResumeLayout(false);
+            this.genreBindingNavigator.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MovieDatabaseDataSet movieDatabaseDataSet;
+        private System.Windows.Forms.BindingSource genreBindingSource;
+        private MovieDatabaseDataSetTableAdapters.GenreTableAdapter genreTableAdapter;
+        private MovieDatabaseDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.BindingNavigator genreBindingNavigator;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
+        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
+        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
+        private System.Windows.Forms.ToolStripButton genreBindingNavigatorSaveItem;
+        private System.Windows.Forms.TextBox genreIdTextBox;
+        private System.Windows.Forms.TextBox genreTextBox;
+    }
+}
---
+++ Champlain Movie Data/Champlain Movie Data/GenreForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Champlain_Movie_Data
+{
+    public partial class GenreForm : Form
+    {
+        public GenreForm()
+        {
+            InitializeComponent();
+        }
+
+        private void genreBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.genreBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.movieDatabaseDataSet);
+
+        }
+
+        private void GenreForm_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'movieDatabaseDataSet.Genre' table. You can move, or remove it, as needed.
+            this.genreTableAdapter.Fill(this.movieDatabaseDataSet.Genre);
+
+        }
+    }
+}
